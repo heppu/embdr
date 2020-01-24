d5d495534dbc2cce286934c0df751233370b9cdf
Henri Koski <henri.t.koski@gmail.com>
2020-01-24 20:38:45 +0200

    Update main

--- cmd/embdr/main.go
+++ cmd/embdr/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"flag"
-	"io"
+	"fmt"
+	"github.com/heppu/embdr"
 	"os"
 )
 
@@ -20,19 +22,36 @@ func main() {
 	flag.BoolVar(&public, "public", false, "makes generared data public")
 	flag.Parse()
 
-	var (
-		r io.Reader
-		w io.Writer
-	)
+	var err error
+	r := os.Stdin
+	w := os.Stdout
 
-	if input == "" {
-		r = os.Stdin
+	if input != "" {
+		if r, err = os.Open(input); err != nil {
+			logAndExit("Couldn't open input file: %s", err)
+		}
+		defer r.Close()
 	}
 
-	if output == "" {
-		w = os.Stdout
+	if output != "" {
+		if r, err = os.Create(output); err != nil {
+			logAndExit("Couldn't create output file: %s", err)
+		}
+		defer w.Close()
 	}
 
-	r.Read([]byte{})
-	w.Write([]byte{})
+	enc, err := embdr.NewEncoder(w)
+	if err != nil {
+		logAndExit("Couldn't create new encoder: %s", err)
+	}
+	defer enc.Close()
+
+	if _, err := bufio.NewReader(r).WriteTo(enc); err != nil {
+		logAndExit("Couldn't encode data: %s", err)
+	}
+}
+
+func logAndExit(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, args...)
+	os.Exit(1)
 }
